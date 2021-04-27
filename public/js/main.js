eb2e0de77ddc80808ca9e6314afd9821d07e24ed
AndyLinxies <linxies@gmail.com>
2021-04-27 21:59:48 +0200

    Exos intro Function

---
+++ public/js/main.js
@@ -0,0 +1,69 @@
+//Exo1
+// function prems(param1,param2) {
+//     return param1+param2
+// }
+// console.log(prems(2,5));
+
+//Exo2
+// function ex2(nb1,nb2) {
+//     return nb2-nb1;
+// }
+// console.log(ex2(5,7));
+
+//Exo3
+// function ex3(par1,par2) {
+//     return par1*par2;
+// }
+// console.log(ex3(5,7));
+
+//Exo4
+// function ex4(par1,par2) {
+//     return par1/par2
+// }
+// console.log(ex4(5,7));
+
+//Exo5
+// function ex5(par1,par2) {
+//     return par1%par2
+// }
+// console.log(ex5(7,7));
+
+//Exo6
+// function ex6(par) {
+//     return Math.sqrt(par)
+// }
+// console.log(ex6(4));
+
+//Exo7
+// function ex7(par1,par2) {
+//         return Math.pow(par1,par2)
+// }
+// console.log(ex7(7,7));
+
+//Exo8
+// function capitalize(par) {
+//     return par.charAt(0).toUpperCase()+par.substring(1)
+// }
+// console.log(capitalize("bonjour"));
+
+//Exo9
+// function calcul(nbr1 , operator , nbr2) {
+//     let resAdd=nbr1+nbr2;
+//     let resSous= nbr1-nbr2;
+//     let resMult=nbr1*nbr2;
+//     let resDiv=nbr1/nbr2
+//     switch (operator) {
+//         case "+":
+//             return resAdd;
+//         case "-":
+//             return resSous;
+//         case "*":
+//             return resMult;
+//         case "/":
+//             return resDiv;
+//         default:
+//          console.log("Erreur")
+//             break;
+//     }
+// }
+// console.log(calcul(4,"*",4));
